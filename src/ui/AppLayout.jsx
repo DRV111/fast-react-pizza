2de698db10992d261b055287038aae5773de3326
DRV111 <xblackcrownx@gmail.com>
2024-02-02 11:28:00 +0300

    feat: add AppLayout, Header components

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,15 @@
+import Header from './Header';
+
+function AppLayout() {
+  return (
+    <div>
+      <Header />
+
+      <main>
+        <h1>main stuff</h1>
+      </main>
+    </div>
+  );
+}
+
+export default AppLayout;
