7983d7eb00f8d3b0671df33b0e12fa1422436b78
DRV111 <xblackcrownx@gmail.com>
2024-02-08 12:15:47 +0300

    refactor: make LinkButton as a reusable component

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,8 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function LinkButton({ children, to }) {
+  const navigate = useNavigate();
+
+  const className = "text-sm text-blue-500 hover:text-blue-600 hover:underline";
+  if (to === "-1")
+    return (
+      <button className={className} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
   return (
-    <Link to={to} className="text-sm text-blue-500 hover:text-blue-600">
+    <Link to={to} className={className}>
       {children}
     </Link>
   );
