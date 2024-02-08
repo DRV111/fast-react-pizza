aca77b621799c292fc462c07202ed7ce7a01fb16
DRV111 <xblackcrownx@gmail.com>
2024-02-08 13:44:17 +0300

    feat: customize tilewind config file

--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,6 +1,6 @@
 function Loader() {
   return (
-    <div className="backdrop-brightness-1 absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm backdrop-brightness-90">
       <div className="loader"></div>
     </div>
   );
